73b9cd993e05c6ddb8820960fdfdc2bcc959f6f2
Ronneh <37001119+Ronneh@users.noreply.github.com>
2024-04-08 19:06:44 +0200

    Event- und Payment Controller erstellt. Event Ordner und Views angelegt. Payment Ordner und HTML angelegt (BezahlView). Layout angepasst wie im alten Projekt.

---
+++ EventManagmentSystem/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagmentSystem.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
