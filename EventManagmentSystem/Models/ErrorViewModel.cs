a25b1976d3eb5e0db8c9af81ba4654dc3bebc3bb
Jsc02 <jennifer.schoen.2002@gmail.com>
2024-04-08 12:10:06 +0200

    Projektdateien hinzufügen.

---
+++ EventManagmentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventManagmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
