3a1eb5cc9bede1fe1b2f3727cd3139e9dbc66634
hadi <abdulhadi.alghawas@webit.de>
2024-04-09 13:50:36 +0200

    SellerController und BuyerController

---
+++ EventManagmentSystem/Controllers/BuyerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagmentSystem.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
