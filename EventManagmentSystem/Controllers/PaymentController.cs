8fd1dcf2fc374c4250b1e81afea095dbce49c675
Yannick Rammelt <yannick.rammelt@smapone.com>
2024-04-12 19:47:11 +0200

    aktueller Stand 12.04.2024

--- EventManagmentSystem/Controllers/PaymentController.cs
+++ EventManagmentSystem/Controllers/PaymentController.cs
@@ -1,12 +1,92 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.RegularExpressions;
+using EventManagmentSystem.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EventManagmentSystem.Controllers
 {
     public class PaymentController : Controller
     {
+        // ToDo: Das hier soll in die Attribute und dann gelöscht werden
+        /*public bool CheckPaymentData()
+        {
+            if (!CheckThatIBANIsInLegitimateForm("DE24100900002648730007"))
+            {
+                return false;
+            }
+            if (!CheckThatNameIsInLegitimateForm("Yannick Rammelt"))
+            {
+                return false;
+            }
+            if (!CheckExpireDate(DateTime.Now))
+            {
+                return false;
+            }
+            if (!CheckThatSafetyNumberIsInLegitimateForm())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CheckThatNameIsInLegitimateForm(string name)
+        {
+            int numberOfBlanks = 0;
+
+            foreach (var character in name.ToCharArray())
+            {
+                if (character >= '0' && character <= '9')
+                {
+                    return false;
+                }
+
+                if (character.Equals(" "))
+                {
+                    numberOfBlanks++;
+                }
+            }
+
+            if (numberOfBlanks == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CheckExpireDate(DateTime expireDateTime)
+        {
+            if (expireDateTime.ToUniversalTime() <= DateTime.Now.ToUniversalTime())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckThatSafetyNumberIsInLegitimateForm()
+        {
+            return true;
+        }*/
+
+        [HttpGet]
         public IActionResult Payment()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Payment(PaymentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Error");
+                return View(model);
+            }
         }
     }
 }
---
+++ EventManagmentSystem/Models/ViewModel/PaymentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EventManagmentSystem.Attributes;
+
+namespace EventManagmentSystem.Models.ViewModel;
+
+public class PaymentViewModel
+{
+    [Required(ErrorMessage = "Please enter your IBAN...")]
+    [Iban(ErrorMessage = "Die IBAN ist fehlerhaft")]
+    [Display(Name = "IBAN")]
+    public string IBAN { get; set; }
+
+    [Required(ErrorMessage = "Please enter the card owner...")]
+    [Display(Name = "Kartenbesitzer")]
+    public string cardOwner { get; set; }
+
+    [Required(ErrorMessage = "Please enter the expiration date of your credit card...")]
+    [Display(Name = "Ablaufdatum")]
+    public DateTime ExpiredDateTime { get; set; }
+
+    [Required(ErrorMessage = "Please enter the security number...")]
+    [Display(Name = "Sicherheitsnummer")]
+    public int securityNumber { get; set; }
+}
+
+// ToDo: Erstelle Attribute für alle weitere Elemente und überprüfe diese
