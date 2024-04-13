04741b7d89584e96b5d86e2f3cc55060c751a04f
Yannick Rammelt <yannick.rammelt@smapone.com>
2024-04-13 10:16:51 +0200

    Löscht IbanAttribute und verlagert die Logik zum Check auf Korrektheit der eingegebenen Zahlungsdaten in den PaymentController. Bindet Eingaben in PaymentView an PaymentViewModel

--- EventManagmentSystem/Controllers/PaymentController.cs
+++ EventManagmentSystem/Controllers/PaymentController.cs
@@ -6,69 +6,6 @@ namespace EventManagmentSystem.Controllers
 {
     public class PaymentController : Controller
     {
-        // ToDo: Das hier soll in die Attribute und dann gelöscht werden
-        /*public bool CheckPaymentData()
-        {
-            if (!CheckThatIBANIsInLegitimateForm("DE24100900002648730007"))
-            {
-                return false;
-            }
-            if (!CheckThatNameIsInLegitimateForm("Yannick Rammelt"))
-            {
-                return false;
-            }
-            if (!CheckExpireDate(DateTime.Now))
-            {
-                return false;
-            }
-            if (!CheckThatSafetyNumberIsInLegitimateForm())
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private bool CheckThatNameIsInLegitimateForm(string name)
-        {
-            int numberOfBlanks = 0;
-
-            foreach (var character in name.ToCharArray())
-            {
-                if (character >= '0' && character <= '9')
-                {
-                    return false;
-                }
-
-                if (character.Equals(" "))
-                {
-                    numberOfBlanks++;
-                }
-            }
-
-            if (numberOfBlanks == 0)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private bool CheckExpireDate(DateTime expireDateTime)
-        {
-            if (expireDateTime.ToUniversalTime() <= DateTime.Now.ToUniversalTime())
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private bool CheckThatSafetyNumberIsInLegitimateForm()
-        {
-            return true;
-        }*/
-
         [HttpGet]
         public IActionResult Payment()
         {
@@ -78,15 +15,124 @@ namespace EventManagmentSystem.Controllers
         [HttpPost]
         public IActionResult Payment(PaymentViewModel model)
         {
-            if (!ModelState.IsValid)
+            bool isErrorInInput = false;
+
+            // ToDo: Hier muss noch Code rein
+            if (!CheckThatIbanIsInLegitimateForm(model.IBAN))
+            {
+                isErrorInInput = true;
+            }
+
+            if (!CheckThatCardOwnerIsInLegitimateForm(model.CardOwner))
+            {
+                isErrorInInput = true;
+            }
+
+            if (!CheckExpireDate(model.ExpiredDateTime))
+            {
+                isErrorInInput = true;
+            }
+
+            if (CheckThatSafetyNumberIsInLegitimateForm(model.SecurityNumber))
+            {
+                isErrorInInput = true;
+            }
+
+            if (isErrorInInput)
             {
                 return View(model);
             }
-            else
+
+            // ToDo: Hier muss noch der Sende-Aufruf hin
+            return View(model);
+        }
+
+
+        public bool CheckThatIbanIsInLegitimateForm(string iban)
+        {
+            
+            string computerUnderstandableIban = Regex.Replace(iban, " ", "");
+            List<char> characterList = iban.ToCharArray().ToList();
+
+            // Hat die Iban die korrekte Anzahl an Zeichen
+            if (!characterList.Count.Equals(22))
             {
-                ModelState.AddModelError(string.Empty, "Error");
-                return View(model);
+                return false;
             }
+
+            // Sind die ersten zwei Zeichen der Iban Buchstaben
+            if (!(char.IsLetter(characterList[0]) && char.IsLetter(characterList[1])))
+            {
+                return false;
+            }
+
+            // Sind alle Zeichen nach den ersten beiden Zahlen
+            foreach (char character in characterList.Skip(2))
+            {
+                if (!(character >= '0' && character <= '9'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool CheckThatCardOwnerIsInLegitimateForm(string name)
+        {
+            // fängt keine Eingabe, als auch Eingaben, die von der minimalen Länge nicht korrekt sein können
+            // Bsp.: "A B" => A = Vorname, B = Nachname => mit Leerzeichen drei Buchstaben nötig
+            if (name.ToCharArray().ToList().Count < 3)
+            {
+                return false;
+            }
+
+            int numberOfBlanks = 0;
+
+            foreach (var character in name.ToCharArray())
+            {
+                if (character.Equals(" "))
+                {
+                    numberOfBlanks++;
+                    break;
+                }
+
+                // Zahlen und Sonderzeichen ausschließen
+                if (!char.IsLetter(character))
+                {
+                    return false;
+                }
+            }
+
+            // Check ob es Vor- und Nachname gibt
+            if (numberOfBlanks <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool CheckExpireDate(DateTime expireDateTime)
+        {
+            // Ablaudatum vor der aktuellen Zeit? => dann illegitimes Ablaufdatum
+            if (expireDateTime.ToUniversalTime() <= DateTime.Now.ToUniversalTime())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckThatSafetyNumberIsInLegitimateForm(int securityNumber)
+        {
+            // Zahl Dreistellig und zwischen 0 und 999
+            if (!(securityNumber < 000 || securityNumber > 999 || securityNumber.ToString().Length != 3 || securityNumber.Equals(null)))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
