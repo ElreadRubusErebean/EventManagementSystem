20844984669b404771db19fb8fa27712c03acb4a
Yannick Rammelt <yannick.rammelt@smapone.com>
2024-04-15 18:17:00 +0200

    Verschiebt die Funktionen zur Überprüfung von dem Controller in einen eigenen Services. Fügt der CheckIban-Methode einen weiteren Bestandteil hinzu.

--- EventManagmentSystem/Controllers/PaymentController.cs
+++ EventManagmentSystem/Controllers/PaymentController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using EventManagmentSystem.Models.ViewModel;
+using EventManagmentSystem.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventManagmentSystem.Controllers
@@ -15,29 +16,31 @@ namespace EventManagmentSystem.Controllers
         [HttpPost]
         public IActionResult Payment(PaymentViewModel model)
         {
+            PaymentCheckerService paymentCheckerService = new PaymentCheckerService();
             bool isErrorInInput = false;
 
             // ToDo: Hier muss noch Code rein
-            if (!CheckThatIbanIsInLegitimateForm(model.IBAN))
+            if (!paymentCheckerService.CheckThatIbanIsInLegitimateForm(model.IBAN))
             {
                 isErrorInInput = true;
             }
 
-            if (!CheckThatCardOwnerIsInLegitimateForm(model.CardOwner))
+            if (!paymentCheckerService.CheckThatCardOwnerIsInLegitimateForm(model.CardOwner))
             {
                 isErrorInInput = true;
             }
 
-            if (!CheckExpireDate(model.ExpiredDateTime))
+            if (!paymentCheckerService.CheckExpireDate(model.ExpiredDateTime))
             {
                 isErrorInInput = true;
             }
 
-            if (CheckThatSafetyNumberIsInLegitimateForm(model.SecurityNumber))
+            if (paymentCheckerService.CheckThatSafetyNumberIsInLegitimateForm(model.SecurityNumber))
             {
                 isErrorInInput = true;
             }
 
+            // Wenn eine Eingabe nicht korrekt war
             if (isErrorInInput)
             {
                 return View(model);
@@ -45,94 +48,6 @@ namespace EventManagmentSystem.Controllers
 
             // ToDo: Hier muss noch der Sende-Aufruf hin
             return View(model);
-        }
-
-
-        public bool CheckThatIbanIsInLegitimateForm(string iban)
-        {
-            
-            string computerUnderstandableIban = Regex.Replace(iban, " ", "");
-            List<char> characterList = iban.ToCharArray().ToList();
-
-            // Hat die Iban die korrekte Anzahl an Zeichen
-            if (!characterList.Count.Equals(22))
-            {
-                return false;
-            }
-
-            // Sind die ersten zwei Zeichen der Iban Buchstaben
-            if (!(char.IsLetter(characterList[0]) && char.IsLetter(characterList[1])))
-            {
-                return false;
-            }
-
-            // Sind alle Zeichen nach den ersten beiden Zahlen
-            foreach (char character in characterList.Skip(2))
-            {
-                if (!(character >= '0' && character <= '9'))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private bool CheckThatCardOwnerIsInLegitimateForm(string name)
-        {
-            // fängt keine Eingabe, als auch Eingaben, die von der minimalen Länge nicht korrekt sein können
-            // Bsp.: "A B" => A = Vorname, B = Nachname => mit Leerzeichen drei Buchstaben nötig
-            if (name.ToCharArray().ToList().Count < 3)
-            {
-                return false;
-            }
-
-            int numberOfBlanks = 0;
-
-            foreach (var character in name.ToCharArray())
-            {
-                if (character.Equals(" "))
-                {
-                    numberOfBlanks++;
-                    break;
-                }
-
-                // Zahlen und Sonderzeichen ausschließen
-                if (!char.IsLetter(character))
-                {
-                    return false;
-                }
-            }
-
-            // Check ob es Vor- und Nachname gibt
-            if (numberOfBlanks <= 0)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private bool CheckExpireDate(DateTime expireDateTime)
-        {
-            // Ablaudatum vor der aktuellen Zeit? => dann illegitimes Ablaufdatum
-            if (expireDateTime.ToUniversalTime() <= DateTime.Now.ToUniversalTime())
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        private bool CheckThatSafetyNumberIsInLegitimateForm(int securityNumber)
-        {
-            // Zahl Dreistellig und zwischen 0 und 999
-            if (!(securityNumber < 000 || securityNumber > 999 || securityNumber.ToString().Length != 3 || securityNumber.Equals(null)))
-            {
-                return false;
-            }
-
-            return true;
         }
     }
 }
---
+++ EventManagmentSystem/Services/PaymentCheckerService.cs
@@ -0,0 +1,132 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace EventManagmentSystem.Services
+{
+    public class PaymentCheckerService
+    {
+        public bool CheckThatIbanIsInLegitimateForm(string iban)
+        {
+            string computerUnderstandableIban = Regex.Replace(iban, " ", "");
+            List<char> characterList = computerUnderstandableIban.ToCharArray().ToList();
+
+            // Hat die Iban die korrekte Anzahl an Zeichen
+            if (!characterList.Count.Equals(22))
+            {
+                return false;
+            }
+
+            if (!(char.IsLetter(characterList[0]) && char.IsLetter(characterList[1])))
+            {
+                return false;
+            }
+
+            foreach (char character in characterList.Skip(2))
+            {
+                if (!(character >= '0' && character <= '9'))
+                {
+                    return false;
+                }
+            }
+
+            // Check That TestDigit is correct
+            int checkDigit = 0;
+
+            for (int index = 0; index <= 3; index++)
+            {
+                if (index <= 1)
+                {
+                    int valueInt = (int)(((characterList[index] % 32) - 1) + 10);
+                    List<char> valueList = valueInt.ToString().ToCharArray().ToList();
+
+                    foreach (var value in valueList)
+                    {
+                        characterList.Add(value);
+                    }
+                    continue;
+                }
+
+                if (index == 2)
+                {
+                    checkDigit += Convert.ToInt32(characterList[index].ToString()) * 10;
+                    continue;
+                }
+                checkDigit += Convert.ToInt32(characterList[index].ToString());
+            }
+
+            for (int index = 0; index < 4; index++)
+            {
+                characterList.RemoveAt(0);
+            }
+
+            string nullExtension = "00";
+            characterList.Add(Convert.ToChar("0"));
+            characterList.Add(Convert.ToChar("0"));
+
+            string resultString = new string(characterList.ToArray());
+            BigInteger result = BigInteger.Parse(resultString);
+            result = 98 - (result % 97);
+
+            if (result != checkDigit)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CheckThatCardOwnerIsInLegitimateForm(string name)
+        {
+            // Bsp.: "A B" => A = Vorname, B = Nachname => mit Leerzeichen drei Buchstaben nötig
+            if (name.ToCharArray().ToList().Count < 3)
+            {
+                return false;
+            }
+
+            int numberOfBlanks = 0;
+
+            foreach (var character in name.ToCharArray())
+            {
+                if (character.Equals(" "))
+                {
+                    numberOfBlanks++;
+                    break;
+                }
+
+                if (!char.IsLetter(character))
+                {
+                    return false;
+                }
+            }
+
+            // Check ob es Vor- und Nachname gibt
+            if (numberOfBlanks <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CheckExpireDate(DateTime expireDateTime)
+        {
+            if (expireDateTime.ToUniversalTime() <= DateTime.Now.ToUniversalTime())
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool CheckThatSafetyNumberIsInLegitimateForm(int securityNumber)
+        {
+            // Zahl Dreistellig und zwischen 0 und 999
+            if (!(securityNumber < 000 || securityNumber > 999 || securityNumber.ToString().Length != 3 || securityNumber.Equals(null)))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
