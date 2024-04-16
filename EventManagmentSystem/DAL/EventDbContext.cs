18d87b05415d65516d15437e47162f45673f8dc2
hadi <abdulhadi.alghawas@webit.de>
2024-04-16 13:05:31 +0200

    refactoring DbContext

--- EventManagmentSystem/DAL/EventDbContext.cs
+++ EventManagmentSystem/DAL/EventDbContext.cs
@@ -13,11 +13,7 @@ namespace EventManagmentSystem.DAL
         //Das brauchen wir später wegen der Beziehung zu der Event-Tabelle und Bookingtable
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Events)  // Annahme, dass eine ICollection<Event> in User definiert ist
-                .WithOne(e => e.User)
-                .HasForeignKey(e => e.UserId)
-                .OnDelete(DeleteBehavior.Cascade);  // Setzt die Löschkaskade
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
