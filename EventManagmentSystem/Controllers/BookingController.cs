5864daf2ba9a7fbdbd30de9b7997e5a4b01f79c4
hadi <abdulhadi.alghawas@webit.de>
2024-04-23 10:48:40 +0200

    Buchung erst nach Payment
    nur wenn User gültige Payment eingaben macht, wird gebucht

--- EventManagmentSystem/Controllers/BookingController.cs
+++ EventManagmentSystem/Controllers/BookingController.cs
@@ -12,30 +12,6 @@ namespace EventManagmentSystem.Controllers
         {
             _bookingService = bookingService;
         }
-
-        [HttpPost]
-        public async Task<IActionResult> Book(int eventId, int numberOfTickets)
-        {
-            // Die Benutzer-ID aus der Sitzung abrufen
-            var userId = Convert.ToInt32(HttpContext.Session.GetString("UserID"));
-
-            // Buchung durchführen
-            bool success = await _bookingService.CreateBooking(eventId, userId, numberOfTickets);
-
-            // Wenn die Buchung erfolgreich war
-            if (success)
-            {
-                SetSuccessMessage("Booking successful.");
-                return RedirectToAction("Index", "Home");
-            }
-            // Wenn die Buchung fehlgeschlagen ist
-            else
-            {
-                SetErrorMessage("Booking failed.");
-                return RedirectToAction("Index", "Home");
-            }
-        }
-
         public async Task<IActionResult> UserBookings()
         {
             var userId = Convert.ToInt32(HttpContext.Session.GetString("UserID"));
--- EventManagmentSystem/Models/ViewModel/PaymentViewModel.cs
+++ EventManagmentSystem/Models/ViewModel/PaymentViewModel.cs
@@ -6,4 +6,9 @@ public class PaymentViewModel
     public string CardOwner { get; set; }
     public DateTime ExpiredDateTime { get; set; }
     public int SecurityNumber { get; set; }
+
+    // Zusätzliche Felder für die Buchungsdetails
+    public int EventId { get; set; }
+    public string UserId { get; set; }
+    public int NumberOfTickets { get; set; }
 }
