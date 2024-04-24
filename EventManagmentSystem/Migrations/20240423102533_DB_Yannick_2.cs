8d5b41c4190fe8d72622a1c8d101781e496fa5f5
Yannick Rammelt <yannick.rammelt@smapone.com>
2024-04-24 19:58:38 +0200

    Fügt Änderungen am Pflichenheft und aus irgendeinem Grund zwei Migrations hinzu🤷‍♂️

---
+++ EventManagmentSystem/Migrations/20240423102533_DB_Yannick_2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagmentSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class DB_Yannick_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
